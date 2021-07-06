405e8d2df03b8dae7a70a2b567b744bb7c9b15dc
Jose Manzano <j.manzano.g@hotmail.com>
2021-07-06 00:59:00 -0500

    Guava and bucket4j lab

--- src/main/kotlin/Main2.kt
+++ src/main/kotlin/Main2.kt
@@ -9,33 +9,35 @@ import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicLong
 import java.util.stream.Collectors.toCollection
 import kotlin.math.abs
+import com.google.common.util.concurrent.RateLimiter
+import java.time.LocalDateTime
 
 
 fun main() {
 
-	val capacity = 1L//5000L/1000
-	val refill = Refill.greedy(500, Duration.ofMinutes(1))
-	val limit = Bandwidth.classic(capacity, refill)
-	//val limit = Bandwidth.simple(capacity, Duration.ofMinutes(1))
-	val bucket = Bucket4j.builder().addLimit(limit).build()
-	var consumerList = generateSequence { 1L }.take(500).toList()
+//	val capacity = 1L//5000L/1000
+//	//val refill = Refill.greedy(capacity, Duration.ofMinutes(1))
+//	//val limit = Bandwidth.classic(capacity, refill)
+//	val limit = Bandwidth.simple(capacity, Duration.ofMinutes(1))
+//	val bucket = Bucket4j.builder().addLimit(limit).build()
+
+	val rateLimiter = RateLimiter.create(500.0/60.0)
+	rateLimiter.rate = 12.0
+	var consumerList = generateSequence { 1 }.take(500).toList()
 	while (1 in consumerList) {
 		println("Pending consumers $consumerList")
-		println("Reminder consumers: ${consumerList.filter { it == 1L }.size}")
+		println("Reminder consumers: ${consumerList.filter { it == 1 }.size}")
 		val attemptTime = AtomicLong(1)
 		consumerList = consumerList.parallelStream().map {
-			if (it != -1L && bucket.tryConsume(abs(it))) {
-				blockingOperation()
+			//val acquire = rateLimiter.acquire()
+			if (it != -1 && rateLimiter.tryAcquire(Duration.ofMillis(50))) {
+				blockingOperation2()
 				-1
 			} else {
-				attemptTime.set(
-					TimeUnit.NANOSECONDS.toMillis(
-						bucket.estimateAbilityToConsume(
-							abs(it)
-						).nanosToWaitForRefill
-					)
-				)
-				if (it != -1L) println(
+//				attemptTime.set(
+//					TimeUnit.SECONDS.toMillis(acquire.toLong())
+//				)
+				if (it != -1) println(
 					"no available tokens please come back later: ${attemptTime.get() / 1000F} seconds"
 				)
 				it
@@ -46,8 +48,9 @@ fun main() {
 	}
 }
 
-fun blockingOperation() {
+fun blockingOperation2() {
 	//val delay = generateSequence(100L) { it + (1..100).random() }.take(50).toList().random()
+	println("Start at:${LocalDateTime.now()}")
 	val delay = (10L..50).random()
 	Thread.sleep(delay)
 	println("Done! elapsed: $delay ms")
